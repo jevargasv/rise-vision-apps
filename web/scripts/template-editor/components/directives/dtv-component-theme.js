c67e8a4ac2d1d2912f307cc7e02d49022a5cc682
Stuart Lees <stuartlees@gmail.com>
2020-01-14 14:29:46 -0500

    Open Theme settings [stage-8]

---
+++ web/scripts/template-editor/components/directives/dtv-component-theme.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('risevision.template-editor.directives')
+  .directive('templateComponentTheme', ['templateEditorFactory',
+    function (templateEditorFactory) {
+      return {
+        restrict: 'E',
+        scope: true,
+        templateUrl: 'partials/template-editor/components/component-theme.html',
+        link: function ($scope, element) {
+          $scope.factory = templateEditorFactory;
+
+          $scope.registerDirective({
+            type: 'rise-data-theme',
+            iconType: 'streamline',
+            icon: 'palette',
+            element: element,
+            show: function () {
+              element.show();
+              $scope.componentId = $scope.factory.selected.id;
+            }
+          });
+
+        }
+      };
+    }
+  ]);
